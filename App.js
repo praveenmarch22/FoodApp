2cc5c69fc4e039d392b7a96c93f7eac344bbd97c
praveenmarch22 <praveen.220302@gmail.com>
2023-11-06 15:50:20 +0530

    new changes

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 const parent=React.createElement(
     "div",
     {id:"parent"},
-   [ React.createElement(
-    "div",{id:"child1"},
-    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-), React.createElement(
-    "div",{id:"child2"},
-    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-)]);
+[
+    React.createElement("div",{id:"child1"},
+[
+    React.createElement("h1",{},"it is a h1 tag"),
+    React.createElement("h1",{},"this is a h2 tag")
+]),
+React.createElement("div",{id:"child2"},
+[
+    React.createElement("h1",{},"it is a h1 tag"),
+    React.createElement("h1",{},"this is a h2 tag")
+])
+]);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
