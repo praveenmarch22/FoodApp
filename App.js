8d360065d259b8ad9b520d0ef48c63eead0f1a4b
praveenmarch22 <praveen.220302@gmail.com>
2023-11-06 17:39:06 +0530

    new changes

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
+import React from "react";
+import  ReactDOM from "react-dom/client";
+
+
 const parent=React.createElement(
     "div",
-    {id:"parent"},"hiii");
+    {id:"parent"},
+[
+    React.createElement("div",{id:"child1"},
+[
+    React.createElement("h1",{},"it is a h1 tag"),
+    React.createElement("h1",{},"this is a h2 tag")
+]),
+React.createElement("div",{id:"child2"},
+[
+    React.createElement("h1",{},"it is a h1 tag"),
+    React.createElement("h1",{},"this is a h2 tag")
+])
+]);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
