d24ef0c1f170fc3ee66c1266362e99e8efad91e5
praveenmarch22 <praveen.220302@gmail.com>
2023-11-11 18:37:52 +0530

    new changes

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
 
-//  React Functional components
-const Title=() => {
-    return (
-    [<h1>namaste using component</h1>,
-     <h2>namste react</h2>]
+const element= <h1 className="heading1" > Hello JSX! </h1>;
 
-)};
-const HeadingComponent=()=>(
-    <div>
-        <Title/>
-         <h1 className="heading">namste react using jsx </h1>
+const p2=(
+    <h1 className="Multi">
+        This is multi-lines jsx !
+    </h1>
+);
+
+const nested=(
+    <div id="container">
+        <h1> This is a nested jsx! </h1>
     </div>
-   
+);
+
+const twoElements=(
+[
+    <h1>hi hello first line</h1>,
+    <h2>Hi hello second line</h2>
+]);
+
+const head=(
+    <div>
+        {element}
+        {p2}
+        {nested}
+        {twoElements}
+    </div>
 );
 
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(head);
 
 
