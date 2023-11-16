8e2169ef94e66eba2c6c31551bd645e6a73d3158
praveenmarch22 <praveen.220302@gmail.com>
2023-11-16 16:16:57 +0530

    new learnings

--- App.js
+++ App.js
@@ -1,37 +1,23 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
 
-const element= <h1 className="heading1" > Hello JSX! </h1>;
-
-const p2=(
-    <h1 className="Multi">
-        This is multi-lines jsx !
+const Title= () => (
+    <h1>
+        Namaste react using title component
     </h1>
 );
 
-const nested=(
-    <div id="container">
-        <h1> This is a nested jsx! </h1>
-    </div>
-);
+const value=123;
 
-const twoElements=(
-[
-    <h1>hi hello first line</h1>,
-    <h2>Hi hello second line</h2>
-]);
-
-const head=(
-    <div>
-        {element}
-        {p2}
-        {nested}
-        {twoElements}
-    </div>
+const TitleComponent= () =>(
+     <h1>
+        {value}
+        Namaste react using functional component
+    </h1>
 );
 
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(head);
+root.render(<TitleComponent></TitleComponent>);
 
 
