850823688f43d75f0e2b5f288670a8df0ee95226
praveenmarch22 <praveen.220302@gmail.com>
2023-11-17 19:41:43 +0530

    new changes

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const Title= () => (
-    <h1>
-        Namaste react using title component
-    </h1>
-);
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <img
+          className="img"
+          alt="logo"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/25539c29532269.55f7d6a0a8c71.jpg"
+        />
+      </div>
+      <div className="nav-bar">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const value=123;
+const Restrauntcard = (props) => {
+  console.log(props);
+  return (
+    <div className="res-card">
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/arvgaf7i7yj6o3pwyhfu"
+        className="res-image"
+      />
+      <h4>{props.resName}</h4>
+      <h5>{props.cuisine}</h5>
+      <h5>4.4 stars</h5>
+      <h5>34 minutes</h5>
+    </div>
+  );
+};
 
-const TitleComponent= () =>(
-     <h1>
-        {value}
-        Namaste react using functional component
-    </h1>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        <Restrauntcard
+          resName="Meghana Foods"
+          cuisine="Biryani, South Indian,Asian"
+        />
+        <Restrauntcard resName="KFC" cuisine="Burgers, Fried Chicken" />
+      </div>
+    </div>
+  );
+};
 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TitleComponent></TitleComponent>);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
